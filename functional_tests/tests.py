f9a6398f3648ddd795cd8fc0ab1b34d110309546
ildark-s <shamief@yandex.ru>
2019-04-19 00:47:57 +0300

    New URL, view for add in exists lists, functional tests passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -85,7 +85,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
-        self.asssertNotEqual(francis_list_url, edith_list_url)
+        self.assertNotEqual(francis_list_url, edith_list_url)
 
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Купить павлиньи перья', page_text)
