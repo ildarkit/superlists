e8bba709e612f2e5d44573a82069c6924f25b302
ildark-s <shamief@yandex.ru>
2019-05-23 00:23:14 +0300

    New form for home_page, simpler tests, break logic

--- lists/views.py
+++ lists/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 
-from lists.models import Item, List
+from .models import Item, List
+from .forms import ItemForm
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
 
 
 def view_list(request, list_id):
