638327bc20f4ef21bfa39aba90d81ee2d8fbe43c
ildark-s <shamief@yandex.ru>
2019-05-25 00:51:13 +0300

    Duplicate item validation in model

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -39,3 +39,17 @@ class InvalidValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
         
+    def test_cannot_add_dublicate_items(self):
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wellies')
+        
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has_error').text,
+            "You've already got this in your list"
+        ))
+        
